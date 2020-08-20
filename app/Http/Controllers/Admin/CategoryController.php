9ab6fb2c336f7599886e546e0c2a13cef2f8ab05
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2020-08-20 22:42:44 +0700

    #22 Add Category | Validations

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Category;
 use App\Section;
 use Session;
+use Image;
 
 class CategoryController extends Controller
 {
@@ -32,8 +33,53 @@ class CategoryController extends Controller
     public function addEditCategpry(Request $request, $id = null) {
         if(!$id) {
             $title = "Add Category";
+            $category = new Category;
         } else {
             $title = "Edit Category";
+        }
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+            // Category Validations
+            $rules = [
+                'category_name' => 'required|regex:/^[pL\s\-]+$/u',
+                'section_id' => 'required',
+                'url' => 'required',
+                'category_image' => 'image',
+            ];
+            $customMessages = [
+                'category_name.required' => 'Name is required',
+                'category_name.regex' => 'Valid Name is required',
+                'section_id.required' => 'Section is required',
+                'url.required' => 'Category Url is required',
+                'category_image.image' => 'Valid Image is required',
+            ];
+            $this->validate($request,$rules,$customMessages);
+
+            if ($request->hasFile('category_image')) {
+                $iamge_tmp = $request->file('category_image');
+                if ($iamge_tmp->isValid()) {
+                    $extension = $iamge_tmp->getClientOriginalExtension();
+
+                    $imageName = rand(111,99999).'.'.$extension;
+                    $iamgePath = 'images/category_images/'.$imageName;
+
+                    Image::make($iamge_tmp)->save($iamgePath);
+                    $category->category_image = $imageName;
+                }
+            }
+
+            $category->parent_id = $data['parent_id'];
+            $category->section_id = $data['section_id'];
+            $category->category_name = $data['category_name'];
+            $category->category_discount = $data['category_discount'];
+            $category->description = $data['description'];
+            $category->url = $data['url'];
+            $category->meta_title = $data['meta_title'];
+            $category->meta_description = $data['meta_description'];
+            $category->meta_keywords = $data['meta_keywords'];
+            $category->status = 1;
+            $category->save();
+            return redirect('admin/categories');
         }
 
         // get all sections
