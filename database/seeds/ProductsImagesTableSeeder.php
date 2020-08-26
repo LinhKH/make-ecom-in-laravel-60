dc358be4ab40c51bb370c6463adf0817c6cddc67
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2020-08-26 21:01:31 +0700

    #45 Product Images in Admin (I) | Create Table | Migration | Seeding

---
+++ database/seeds/ProductsImagesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('producs_images')->truncate();
+        $productImagesRecords = [
+            [
+                'id' => 1, 'product_id' => 1, 'image' => 'images.jpg-14353.jpg', 'status' => 1
+            ],
+            [
+                'id' => 2, 'product_id' => 1, 'image' => 'Capture001.png-83591.png', 'status' => 1
+            ],
+            [
+                'id' => 3, 'product_id' => 1, 'image' => 'no-image.png', 'status' => 1
+            ],
+        ];
+
+        DB::table('producs_images')->insert($productImagesRecords);
+    }
+}
