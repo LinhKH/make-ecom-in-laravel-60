7352173d22574cb445ef5ff0ad40aefda1b21f95
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2020-08-29 16:32:54 +0700

    #56 Make Featured Products Dynamic (I) | Home Page

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -10,6 +11,9 @@ class IndexController extends Controller
     public function index() {
         $page_name = "index";
 
-        return view('front.index')->with(compact('page_name'));
+        $featuredItems = Product::where('is_featured','Yes')->get()->toArray();
+        $featuredItemsChunk = array_chunk($featuredItems,4);
+
+        return view('front.index')->with(compact('page_name','featuredItemsChunk'));
     }
 }
